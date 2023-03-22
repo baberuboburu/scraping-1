f4cfac37fd0f2763b1cf6d502c7a1e3258a3b673
Aoto Sasaki <aoto.sasaki@eitoss.com>
2023-03-22 17:37:05 +0900

    2回目のコミット

--- toExcel_6.py
+++ toExcel_6.py
@@ -1,8 +1,5 @@
 import pandas as pd
 
 df = pd.read_csv('csv/data_6.csv')
-labels = ['写真URL','施設名','エリア','住所','電話番号','営業時間','対応ジャンル','公式サイト','Youtube']
-df_firstRow = df.columns
-df.columns = labels
-df.iloc[0] = df_firstRow
+df = df.drop(4956)
 df.to_excel('excel/data_6.xlsx')
